f41b56a4ba96e8870ba0f2d097013d628583aaa0
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-04 09:46:02 +0200

    *Added about blade

---
+++ resources/views/about.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+@section('content')
+    <div class="container mt-5">
+        <div class="row align-items-start">
+
+            <div class="col-md-4 text-center mb-4">
+                <img class="img-fluid rounded-3" style="max-width: 100%;" src="{{ asset('storage/images/ProfileImageAnja.jpg') }}" alt="Profilna slika">
+                <p>Anja Marković</p>
+            </div>
+
+            <div class="col-md-8">
+                <h4 class="text-start">
+                    Ja sam Web Developer koji uživa u kreiranju web stranica. Iako sam završila medicinsku školu,
+                    pronašla sam svoju strast u programiranju i sada radim na razvoju frontenda i backenda koristeći
+                    HTML, CSS, Bootstrap, PHP i Laravel. <br><br>
+
+                    Učim svakodnevno i volim da rešavam probleme. Cilj mi je da radim u timu gde mogu da doprinosim i učim još više.
+                    Moje putovanje u programiranje počelo je kada sam na 3. godini studija Stomatološkog fakulteta shvatila da ja
+                    učim to gradivo na "silu". Još tada, mi je informatika u 1. i 2. godini studija veoma privukla pažnju, rado sam
+                    je učila i polagala sa visokom ocenom. <br><br>
+
+                    Nakon što sam postala supruga i majka, i rada u privatnoj poliklinici kao medicinska sestra, shvatila sam da sam
+                    postala previše senzitivna i da medicina nije za mene. <br><br>
+
+                    Razmišljajući šta ja to volim da radim i kojim poslom bih se sa uzbuđenjem i radošću bavila, došla sam do
+                    odgovora, uzimajući u obzir i moju kreativnu stranu, da je to definitivno programiranje. <br><br>
+
+                    Samostalno sam istraživala koji, kako, zašto programski jezik učiti. Prvi korak u tom pravcu je bio sajt The
+                    Odin Project, gde sam savladala osnove HTML-a i CSS-a. Nakon toga sam naišla na sajt IT Mentorstva gde sam uz pomoć
+                    mentora - Tomislava Nikolića krenula put PHP-a i Laravel-a.
+                </h4>
+            </div>
+
+        </div>
+    </div>
+@endsection
