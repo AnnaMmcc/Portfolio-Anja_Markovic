f2fe4f7e79e7d8013b19cccfa21918c55beedce8
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-04 15:05:51 +0200

    *Updated Projects Blade

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -17,5 +17,29 @@
             </a>
         </div>
     </div>
+    <div>
+            <section class="container my-5">
+                <h2 class="mb-4 text-center">Moji Projekti</h2>
+                <div class="row">
+                    @foreach ($projects as $project)
+                        <div class="col-md-4 mb-4">
+                            <div class="card h-100 shadow-sm">
+                                <img src="{{ asset('images/projects/' . $project['image']) }}" class="card-img-top" alt="{{ $project['title'] }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $project['title'] }}</h5>
+                                    <p class="card-text">{{ $project['description'] }}</p>
+                                    <p class="text-muted"><strong>Tehnologije:</strong> {{ $project['technologies'] }}</p>
+                                </div>
+                                @if(!empty($project['link']))
+                                    <div class="card-footer text-center">
+                                        <a href="{{ $project['link'] }}" class="btn btn-primary" target="_blank">Pogledaj projekat</a>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </section>
+    </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::view('/about', 'about');
-Route::get('/projects', \App\Http\Controllers\PortfolioController::class, 'projects');
 Route::get('/contact', [\App\Http\Controllers\PortfolioController::class, 'show'])->name('contact.show');
 Route::post('/contact-send', [\App\Http\Controllers\PortfolioController::class, 'send'])->name('contact.send');
-
+Route::get('/projects', [\App\Http\Controllers\PortfolioController::class, 'projects']);
