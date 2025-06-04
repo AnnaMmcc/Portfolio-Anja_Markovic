93585593397e50e2b18b14a25118aa8bc87967c7
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-04 10:08:29 +0200

    *Updated contact blade, added Mail ContactMessage

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
-    //
+
+    public function show()
+    {
+        return view('contact');
+    }
+
+    public function send(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
+        Mail::to('anja.poslovni.srb@gmail.com')->send(new ContactMessage($data));
+
+        return back()->with('success', 'Poruka je uspešno poslata!');
+    }
 }
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+    <div>
+       <h4>Mozete me kontaktirati</h4>
+    </div>
+@endsection
