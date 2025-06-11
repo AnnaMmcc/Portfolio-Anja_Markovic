058c30fef47cd0eb596301ffdef069b5c4f32ee4
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-11 15:38:38 +0200

    *Added style css to projects blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     <title>Portfolio</title>
 </head>
