8af82ca9ad84dbc8d0f246dacbf34924026809ef
AnnaMmcc <urosevicanja913@gmail.com>
2025-06-11 15:57:43 +0200

    *Added style css to welcome blade

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,12 +14,12 @@
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Ime</label>
-            <input type="text" name="name" class="form-control" required value="{{ old('name') }}">
+            <input type="text" name="name" class="form-control" id="name" required value="{{ old('name') }}">
         </div>
 
         <div class="mb-3">
             <label for="email" class="form-label">Email adresa</label>
-            <input type="email" name="email" class="form-control" required value="{{ old('email') }}">
+            <input type="email" name="email" class="form-control" id="email" required value="{{ old('email') }}">
         </div>
 
         <div class="mb-3">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,34 @@
 @extends('layout')
 @section('content')
-<div class="d-flex flex-column-reverse flex-md-row align-items-center p-3 container">
-    <div class="p-2">
-        <img src="{{ asset('storage/images/woman-1459220_640.png') }}" alt="Anja Markovic slika" class="img-thumbnail rounded float-left m-3" style="width: 300px; height: 350px">
-    </div>
-    <div class="p-2">
-        <h3 class="m-4">Zdravo, ja sam Anja Marković - Web Developer</h3>
-        <h5><i>Samostalni frontend i backend developer sa iskustvom u izradi web stranica koristeći HTML, CSS,<br>
-                Bootstrap, PHP i Laravel. Strastvena prema pisanju čistog koda i učenju novih tehnologija.<br>
-                Tražim priliku za posao u IT industriji gde bih mogla da doprinesem svojim znanjem i nastavim da učim.
-            </i></h5>
-        <a href="/about" type="button" class="btn btn-outline-secondary fs-3 text-dark mt-4">Priča o meni</a>
-    </div>
+    <section class="about-section py-5">
+        <div class="container">
+            <div class="d-flex flex-column-reverse flex-lg-row align-items-center justify-content-between gap-5">
 
-</div>
+                <div class="about-text">
+                    <h2 class="fw-bold mb-3" style="color: #f0c040;">
+                        Zdravo, ja sam Anja Marković – Web Developer
+                    </h2>
+                    <p class="text-muted fs-5 mb-4">
+                        Samostalni frontend i backend developer sa iskustvom u izradi web stranica koristeći
+                        <strong>HTML</strong>, <strong>CSS</strong>, <strong>Bootstrap</strong>, <strong>PHP</strong> i <strong>Laravel</strong>.<br>
+                        Strastvena sam prema pisanju čistog koda i učenju novih tehnologija.<br>
+                        Tražim priliku za posao u IT industriji gde mogu doprineti znanjem i nastaviti da rastem.
+                    </p>
+                    <a href="/about" class="btn btn-warning btn-lg px-4 text-dark shadow-sm">
+                        Priča o meni
+                    </a>
+                </div>
+
+              
+                <div class="about-image text-center">
+                    <img src="{{ asset('storage/images/woman-1459220_640.png') }}"
+                         alt="Anja Markovic"
+                         class="img-fluid rounded shadow"
+                         style="max-width: 320px;">
+                </div>
+
+            </div>
+        </div>
+    </section>
 @endsection
 
